b41e7924cc8da9a7c04929727a1fc79625e7b438
Jojo <jonas.uliana@passwd.com.br>
2022-01-25 10:46:43 -0300

    chore: added projects

---
+++ Automatic.Spoon.Service/Program.cs
@@ -0,0 +1,10 @@
+using Automatic.Spoon.Service;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+await host.RunAsync();
---
+++ Automatic.Spoon.Shared/Class1.cs
@@ -0,0 +1,5 @@
+﻿namespace Automatic.Spoon.Shared;
+public class Class1
+{
+
+}
